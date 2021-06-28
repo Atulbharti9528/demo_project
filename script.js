462d1cb09c884392506e2dd72a2511352fdd04e9
atul bharti <atulbharti9528@gmail.com>
2021-06-28 12:22:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function generatorcat()
+{
+   var image = document.createElement('img');
+   var div = document.getElementById('flex-box-gen');
+    image.src = "https://source.unsplash.com/200x200/?cat";
+  
+    div.appendChild(image); 
+
+
+}
